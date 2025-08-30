43712c8e07909704ef9cfd6eac8a6a6aef6bc463
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-30 06:38:23 +0100

    feat: initial commit for webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'MinimalistWebUI',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  mode: 'development'
+};
