452ae89e1abe228c2fe08ed63441c2e30542f272
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-30 06:48:18 +0100

    Add comments to clarify webpack configuration settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
 const path = require('path');
 
 module.exports = {
+  // Entry point of the application
   entry: './src/index.js',
   output: {
+    // Output filename and path configuration
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
+    // Library settings for UMD compatibility
     library: 'MinimalistWebUI',
     libraryTarget: 'umd'
   },
   module: {
     rules: [
       {
+        // Transpile JavaScript files except those in node_modules
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: /node_modules/, 
         use: 'babel-loader'
       }
     ]
   },
+  // Set mode to development for better debugging
   mode: 'development'
 };
